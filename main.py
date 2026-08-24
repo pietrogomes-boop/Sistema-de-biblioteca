5f7a0b77a85e15a281a2d95e09a538ddfe2e4cc0
Pietro <pietro_a_gomes@estudante.sesisenai.org.br>
2026-08-24 10:50:46 -0300

    Primeiro Commit

---
+++ main.py
@@ -0,0 +1,14 @@
+#exemplo com média
+nome = input("Nome do aluno: ")
+
+nota1 = float(input("Digite a primeira nota: "))
+nota2 = float(input("Digite a segunda nota: "))
+
+media = (nota1 + nota2) / 2
+
+print("Resultado")
+
+print(f"Aluno: {nome}")
+print(f"Nota 1: {nota1}")
+print(f"Nota 2: {nota2}")
+print(f"Media: {media}")
